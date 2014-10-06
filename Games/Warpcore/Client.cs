9405ef037ca40d2aa169da8547cfc974954a44e4
LaylConway <laylconway@live.com>
2014-10-06 12:32:40 +0200

    Added stubs for the first pass of the entity-component-system pattern implementation.

--- Games/Warpcore/Client.cs
+++ Games/Warpcore/Client.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using WarpEngine;
+using WarpEngine.Graphics;
 
 namespace Warpcore
 {
 	public sealed class Client : Game
 	{
+		private readonly World _world = new World();
+
 		public Client()
 		{
+			var atlas = new SpriteAtlas();
+			var playerSprite = atlas.CreateSprite("./Graphics/player.png");
+			atlas.Generate();
+
+			var playerEntity = new Entity();
+			playerEntity.Add(new TransformComponent());
+			playerEntity.Add(new SpriteComponent(playerSprite));
+			playerEntity.Add(new PlayerComponent());
+			_world.Add(playerEntity);
+
 			// Create and set up our update loop (Twice the average framerate)
 			StartLoop(Update, TimeSpan.FromSeconds(1.0/120.0));
 
